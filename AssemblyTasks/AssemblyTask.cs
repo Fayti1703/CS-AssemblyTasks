f030363b008e2c420ded80cd6607c09962612f34
Fayti1703 <fayti1703@protonmail.com>
2024-02-11 23:27:47 +0100

    Add Exception handling to AssemblyTask

--- AssemblyTasks/AssemblyTask.cs
+++ AssemblyTasks/AssemblyTask.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Text;
 using JetBrains.Annotations;
 using Microsoft.Build.Framework;
 using Mono.Cecil;
@@ -18,10 +20,59 @@ abstract public class AssemblyTask : ITask {
 
 
 	public bool Execute() {
-		using AssemblyDefinition assembly = this.ReadAssembly();
-		this.HandleAssembly(assembly);
-		this.WriteAssembly(assembly);
-		return true;
+		try {
+			using AssemblyDefinition assembly = this.ReadAssembly();
+			this.HandleAssembly(assembly);
+			this.WriteAssembly(assembly);
+			return true;
+		} catch(Exception e) {
+			this.LogExceptionError(e);
+			return false;
+		}
+	}
+
+	protected void LogExceptionError(Exception exception, string? errorCode = null) {
+		if(exception is AggregateException aggregate) {
+			foreach(Exception inner in aggregate.Flatten().InnerExceptions) {
+				this.LogExceptionError(inner);
+			}
+		}
+
+		bool showTraces = Environment.GetEnvironmentVariable("FAY_TASK_EXCEPTION_TRACE") != null;
+		StringBuilder messageBuilder = new(300);
+		do {
+			messageBuilder.Append(exception.GetType().Name).Append(": ").Append(exception.Message);
+			if(showTraces)
+				messageBuilder.Append("\n").Append(exception.StackTrace);
+			if(exception.InnerException != null) {
+				messageBuilder.Append("\nInner Exception:\n");
+				exception = exception.InnerException;
+			} else
+				break;
+		} while(exception != null);
+
+		this.LogTaskError(
+			null,
+			errorCode ?? "FYA1000",
+			messageBuilder.ToString(),
+			null
+		);
+	}
+
+	protected void LogTaskError(string? subCategory, string errorCode, string message, string? helpKeyword) {
+		this.BuildEngine.LogErrorEvent(new BuildErrorEventArgs(
+			subCategory,
+			errorCode,
+			this.BuildEngine.ProjectFileOfTaskNode,
+			this.BuildEngine.LineNumberOfTaskNode,
+			this.BuildEngine.ColumnNumberOfTaskNode,
+			0,
+			0,
+			message,
+			helpKeyword,
+			this.GetType().Name,
+			DateTime.Now
+		));
 	}
 
 	virtual protected AssemblyDefinition ReadAssembly() {
